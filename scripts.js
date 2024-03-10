61ed44ea622283b5fa6474e002baf33a921fa240
Nathan Kim <n33937@berkeley.edu>
2024-03-09 17:05:32 -0800

    need javascript

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function toggleTheme() {
+    const body = document.body;
+    const currentTheme = body.getAttribute('data-theme');
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+    body.setAttribute('data-theme', newTheme);
+}
